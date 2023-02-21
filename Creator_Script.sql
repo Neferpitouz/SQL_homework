0bf7f3633a32f91c2c211de9e663cce246e92310
Averetium <me@deiner.ru>
2023-02-21 10:08:14 +0300

    Creator script done

---
+++ Creator_Script.sql
@@ -0,0 +1,52 @@
+CREATE table  if not exists genres (
+  id SERIAL primary key, 
+  name VARCHAR(60) not null
+); 
+
+create table if not exists performers (
+  id SERIAL primary key, 
+  nickname VARCHAR(60) not null unique
+);
+
+create table if not exists genres_of_performers (
+  genre_id SERIAL,
+  foreign key(genre_id) references genres(id),
+  performer_id SERIAL,
+  foreign key(performer_id) references performers(id)
+);
+
+create table if not exists performers_albums (
+  id SERIAL primary key,
+  album_name VARCHAR(60) not null unique references performers(nickname),
+  year_of_release integer not null,
+  check (year_of_release >=1600)
+);
+
+create table if not exists performers_and_albums (
+  performer_id SERIAL,
+  foreign key(performer_id) references performers(id),
+  album_id SERIAL,
+  foreign key(album_id) references performers_albums(id)
+);
+
+create table if not exists tracks (
+  id SERIAL primary key,
+  name VARCHAR(60) not null,
+  lenght integer not null,
+  check (lenght >= 1),
+  album_name VARCHAR(60) not null references performers_albums(album_name)
+);
+
+create table if not exists collections (
+  id SERIAL primary key,
+  name VARCHAR(60) not null,
+  year integer not null,
+  check (year >= 1600)
+);
+
+create table if not exists collection_of_tracks (
+  track_id SERIAL,
+  foreign key(track_id) references tracks(id),
+  collection_id SERIAL,
+  foreign key(collection_id) references collections(id)
+);
